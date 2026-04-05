46501dcc7d77bd5d697bba169d96d0bcbeb2d3f2
Shantnu Gupta <shantnugupta44@gmail.com>
2026-04-05 15:33:05 +0530

    task 3

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
-const form = document.getElementById("form");
-const password = document.getElementById("password");
-const confirmPassword = document.getElementById("confirmPassword");
-const errorMsg = document.getElementById("errorMsg");
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault(); // form submit rokna
+const input = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const list = document.getElementById("taskList");
 
-    if (password.value === confirmPassword.value) {
-        // Success message
-        message.style.color = "green";
-        message.textContent = "Registration Successful!";
+function renderTasks() {
+    list.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            <span class="${task.completed ? 'completed' : ''}">
+                ${task.text}
+            </span>
+            <button class="delete-btn">X</button>
+        `;
+
         
-        // optional: form reset
-        form.reset();
-    } else {
-        // Error message
-        message.style.color = "red";
-        message.textContent = "Passwords do not match!";
-    }
+        li.querySelector("span").addEventListener("click", () => {
+            tasks[index].completed = !tasks[index].completed;
+            renderTasks();
+        });
+
+        
+        li.querySelector("button").addEventListener("click", () => {
+            tasks.splice(index, 1);
+            renderTasks();
+        });
+
+        list.appendChild(li);
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+
+addBtn.addEventListener("click", () => {
+    if (input.value.trim() === "") return;
+
+    tasks.push({
+        text: input.value,
+        completed: false
+    });
+
+    input.value = "";
+    renderTasks();
 });
+
+renderTasks();
